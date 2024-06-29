df9da2e9a20cf5ab223dae55645741cab0c5ee29
Svetlana Movchan <movchan.svetlana07@gmail.com>
2024-06-29 14:22:10 +0300

    Added CSV files and modified SQL queries

--- queries.sql
+++ queries.sql
@@ -71,3 +71,61 @@ SELECT
     END AS day_of_week,
     income
 FROM sales_summary
+
+
+--Возрастные группы покупателей
+
+SELECT
+    CASE
+        WHEN age BETWEEN 16 AND 25 THEN '16-25'
+        WHEN age BETWEEN 26 AND 40 THEN '26-40'
+        ELSE '40+'
+    END AS age_category,
+    COUNT(customer_id) AS age_count
+FROM customers
+GROUP BY age_category
+ORDER BY age_category
+
+
+---Количество покупателей и выручка по месяцам
+
+SELECT
+    TO_CHAR(s.sale_date, 'YYYY-MM') AS selling_month,
+    COUNT(DISTINCT s.customer_id) AS total_customers,
+    FLOOR(SUM(s.quantity * p.price)) AS income
+FROM sales AS s
+LEFT JOIN products AS p
+    ON s.product_id = p.product_id
+GROUP BY 1
+ORDER BY selling_month
+
+
+--Покупатели, чья первая покупка пришлась на время проведения специальных акций
+
+WITH sales_data AS (
+    SELECT
+        s.customer_id,
+        CONCAT(c.first_name, ' ', c.last_name) AS customer,
+        s.sale_date,
+        p.price,
+        CONCAT(e.first_name, ' ', e.last_name) AS seller,
+        ROW_NUMBER()
+            OVER (PARTITION BY s.customer_id ORDER BY s.sale_date)
+        AS row
+    FROM sales AS s
+    LEFT JOIN products AS p
+        ON s.product_id = p.product_id
+    LEFT JOIN customers AS c
+        ON s.customer_id = c.customer_id
+    LEFT JOIN employees AS e
+        ON s.sales_person_id = e.employee_id
+    ORDER BY 1
+)
+
+SELECT
+    customer,
+    sale_date,
+    seller
+FROM sales_data
+WHERE row = 1 AND price = 0
+
